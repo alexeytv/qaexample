019e8661f7015b7b5c516442810925528cfe3256
alexeytv <me@alexey.tv>
2023-11-12 15:23:39 +0000

    First commit

---
+++ config/links.py
@@ -0,0 +1,7 @@
+
+
+class Links:
+    HOST = "https://kitaj.ru/"
+    LOGIN_PAGE = f"{HOST}wp-login.php"
+    DASHBOARD_PAGE = f"{HOST}my-account/"
+    ACCOUNT_PAGE = f"{HOST}my-account/edit-account/"
